0ffca48330805f5b308d8efe7f6ec6ae84bd92ad
e5636u <florian.chevalier2@etu.univ-lorraine.fr>
2026-02-16 08:14:00 +0100

    commit changes to do the chechout

--- src/test/java/com/example/testit/service/TaskServiceIT.java
+++ src/test/java/com/example/testit/service/TaskServiceIT.java
@@ -1,9 +1,8 @@
 package com.example.testit.service;
 
-import com.example.testit.adapter.mail.MailService;
 import com.example.testit.model.Task;
 import com.example.testit.repository.TaskRepository;
-import com.example.testit.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,25 +11,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 @SpringBootTest
+@Transactional
 public class TaskServiceIT {
-
-    @Autowired
-    UserRepository userRepository;
 
     @Autowired
     TaskRepository taskRepository;
 
     @Autowired
-    MailService mailService;
-
-    @Autowired
     TaskService taskService;
+
+    @Test
+    public void testFindById() {
+        var task = new Task();
+        task.setTitle("Task to find");
+        task.setDescription("Description");
+        var taskSaved = taskRepository.save(task);
+
+        var foundTask = taskService.findById(taskSaved.getId());
+
+        Assertions.assertThat(foundTask).isPresent().get().usingRecursiveComparison().isEqualTo(taskSaved);
+    }
 
     @Test
     public void testDeleteTask() {
         var task = new Task();
 
-        task.setTitle("task1");
+        task.setTitle("task to delete");
         task.setDescription("This is the frist task");
         var taskSaved = taskRepository.save(task);
 
